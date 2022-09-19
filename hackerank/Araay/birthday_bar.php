d9fe25e7a12616761a927f5c583e21f6b0a71d0d
MimMostakim <39443069+MimMostakim@users.noreply.github.com>
2022-09-19 21:51:06 +0600

    Add files via upload

---
+++ hackerank/Araay/birthday_bar.php
@@ -0,0 +1,30 @@
+<?php
+function birthday($s, $d, $m) {
+    for ($i = 0, $len = count($s), $result = 0; $i < $len; $i++) {
+        $arr = array_slice($s, $i, $m);
+        if (count($arr) == $m && array_sum($arr) == $d) {
+            ++$result;
+        }
+    }
+    return $result;
+}
+
+$fptr = fopen(getenv("outtput2.txt"), "w");
+
+$n = intval(trim(fgets(STDIN)));
+
+$s_temp = rtrim(fgets(STDIN));
+
+$s = array_map('intval', preg_split('/ /', $s_temp, -1, PREG_SPLIT_NO_EMPTY));
+
+$dm = explode(' ', rtrim(fgets(STDIN)));
+
+$d = intval($dm[0]);
+
+$m = intval($dm[1]);
+
+$result = birthday($s, $d, $m);
+
+fwrite($fptr, $result . "\n");
+
+fclose($fptr);
